546ee308bb3298363734b54352736b4543d31076
matheusaguiar <joaomatheusaguiar@gmail.com>
2017-03-24 15:57:17 -0300

    [Aguiar] xml da tela de busca

--- app/src/main/java/unifor/com/br/findclass/fragments/BuscaFragment.java
+++ app/src/main/java/unifor/com/br/findclass/fragments/BuscaFragment.java
@@ -1,6 +1,5 @@
 package unifor.com.br.findclass.fragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,21 +8,13 @@ import android.view.ViewGroup;
 
 import unifor.com.br.findclass.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class BuscaFragment extends Fragment {
-
-
-    public BuscaFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_busca, container, false);
     }
+
 
 }
