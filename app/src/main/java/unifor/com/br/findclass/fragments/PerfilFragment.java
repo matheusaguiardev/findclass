91418878f313a910b54f3b52c7ecb634a5ab1e81
Thiago Alonso <thiago.alonso@edu.unifor.br>
2017-03-25 10:45:17 -0300

    TextViews e imagem da tela de perfil

--- app/src/main/java/unifor/com/br/findclass/fragments/PerfilFragment.java
+++ app/src/main/java/unifor/com/br/findclass/fragments/PerfilFragment.java
@@ -6,13 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import unifor.com.br.findclass.R;
+import unifor.com.br.findclass.views.MainActivity;
 
 public class PerfilFragment extends Fragment {
-    @Nullable
+
+    private TextView nomeUsuario;
+    private TextView matriculaUsuario;
+
+    MainActivity myActivity;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+
+        View visao = inflater.inflate(R.layout.fragment_perfil, container , false);
+        myActivity = (MainActivity) getActivity();
+
+        nomeUsuario = (TextView) visao.findViewById(R.id.textView_nome1);
+        matriculaUsuario = (TextView) visao.findViewById(R.id.textView_matricula1);
+
+
         return inflater.inflate(R.layout.fragment_perfil, container, false);
     }
 }
