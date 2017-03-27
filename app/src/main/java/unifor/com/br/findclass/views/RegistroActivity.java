c20b7b783b02cb076c93e79d292e7703b537abfb
Thiago Alonso <thiago.alonso@edu.unifor.br>
2017-03-26 23:46:06 -0300

    Layout tela de Registro_correção

---
+++ app/src/main/java/unifor/com/br/findclass/views/RegistroActivity.java
@@ -0,0 +1,25 @@
+package unifor.com.br.findclass.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import unifor.com.br.findclass.R;
+
+/**
+ * Created by thiago on 26/03/17.
+ */
+
+public class RegistroActivity extends GenericActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+
+    }
+}
